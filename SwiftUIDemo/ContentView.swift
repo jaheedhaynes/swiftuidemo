e853b2d4729e75e1ca9be5dfc385a4bd69a96916
Jaheed Haynes <jaheedhaynes@Jaheeds-MacBook-Pro.local>
2019-09-24 16:03:08 -0400

    i added this

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Jaheed Haynes on 9/24/19.
+//  Copyright © 2019 Jaheed Haynes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
